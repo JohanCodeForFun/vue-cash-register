16036319bbc3cff0cf782df53289decf38fc570e
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-20 21:32:33 +0100

    created function to fetch api products and show to user on screen with title, category and images

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,10 +1,14 @@
 <template>
-<div :key="item.id" v-for="item in productCategories">
-<p>{{ item.id }}: {{ item.text }}, price: {{ item.price }}</p>
+<div :key="item" v-for="item in productCategories">
+<h3>{{ item.title }}</h3>
+<p>{{ item.id }}, {{ item.category }}</p>
+<img v-bind:src="item.image" alt="">
 </div>
 </template>
 
 <script>
+// const api = 'https://fakestoreapi.com/';
+
 export default {
   name: 'ProductCategories',
   data() {
@@ -12,13 +16,28 @@ export default {
       productCategories: [],
     };
   },
-  created() {
-    this.productCategories = [
-      { id: '1', text: 'electronics', price: 100 },
-      { id: '2', text: 'jewelery', price: 100 },
-      { id: '3', text: "men's clothing", price: 100 },
-      { id: '3', text: "women's clothing", price: 100 },
-    ];
+  methods: {
+    async fetchCategories() {
+      const res = await fetch('https://fakestoreapi.com/products');
+      const data = await res.json();
+      console.log(data);
+      return data;
+    },
+  },
+  async created() {
+    this.productCategories = await this.fetchCategories();
+    // this.productCategories = [
+    //   { id: '1', text: 'electronics', price: 100 },
+    //   { id: '2', text: 'jewelery', price: 100 },
+    //   { id: '3', text: "men's clothing", price: 100 },
+    //   { id: '3', text: "women's clothing", price: 100 },
+    // ];
   },
 };
 </script>
+
+<style>
+img {
+width: 100%;
+}
+</style>
