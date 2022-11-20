f45bcce61c8494a42e47be5ebc25dae73bc1d1a6
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-20 20:47:37 +0100

    fixed output of array from object to display on ProductCategories

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,15 +1,27 @@
 <template>
-test test
-<div :key="product.id" v-for="product in ProductCategories">
-<p>{{ product.title }}</p>
+<div :key="item.id" v-for="item in productCategories">
+<p>{{ item.id }}: {{ item.text }}</p>
 </div>
 </template>
 
 <script>
 export default {
   name: 'ProductCategories',
-  props: {
-    categories: Array,
+  // props: {
+  //   // productCategories: Array,
+  // },
+  data() {
+    return {
+      productCategories: [],
+    };
+  },
+  created() {
+    this.productCategories = [
+      { id: '1', text: 'electronics' },
+      { id: '2', text: 'jewelery' },
+      { id: '3', text: "men's clothing" },
+      { id: '3', text: "women's clothing" },
+    ];
   },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="home">
-    <h1>What are you looking for?</h1>
-    <ProductCategories :ProductCategories="ProductCategories"/>
+    <h2>What are you looking for?</h2>
+    <!-- <ProductCategories :categories="categories"/> -->
   </div>
+  <ProductCategories />
 </template>
 
 <script>
@@ -14,28 +15,9 @@ export default {
   components: {
     ProductCategories,
   },
-  methods: {
-    async getCategories() {
-      const res = await fetch('https://fakestoreapi.com/products/categories');
-      const { results } = await res.json();
-      console.log(results);
-    },
-  },
-  data() {
-    return {
-      categories: [],
-    };
-  },
-  created() {
-    this.categories = [
-      { id: '1', text: 'electronics' },
-      { id: '2', text: 'jewelery' },
-      { id: '3', text: "men's clothing" },
-      { id: '3', text: "women's clothing" },
-    ];
-  },
 };
 </script>
 
 <style>
+
 </style>
