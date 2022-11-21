0d5e7f4648defd6daed9aadea629535a2a1cc071
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-21 21:47:03 +0100

    all products displays on page. uncaugth typeError on mustashe syntax.

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,43 +1,25 @@
 <template>
-<!-- <div>{{ getCategories }}</div> -->
-<!-- <button @click="getCategories()">get getCategories</button> -->
-<h2>{{ categoryMensClothes.title }}</h2>
-<img v-bind:src="categoryMensClothes.image" :alt="categoryMensClothes.title" class="categoryThumb">
-<h2>{{ categoryWomensClothes }}</h2>
-<img v-bind:src="categoryWomensClothesImage" alt="" class="categoryThumb">
-<h2>{{ categoryElectronics }}</h2>
-<img v-bind:src="categoryElectronicsImage" alt="" class="categoryThumb">
-<h2>{{ categoryJewelry }}</h2>
-<img v-bind:src="categoryJewelryImage" alt="" class="categoryThumb">
+<h2>{{ catMensClothes.category }}</h2>
+<img v-bind:src="catMensClothes.image" :alt="catMensClothes.title" class="categoryThumb">
+<h2>{{ categoryJewelry.category }}</h2>
+<img v-bind:src="categoryJewelry.image" :alt="categoryJewelry.title" class="categoryThumb">
+<h2>{{ categoryElectronics.category }}</h2>
+<img v-bind:src="categoryElectronics.image" :alt="categoryElectronics.title" class="categoryThumb">
+<h2>{{ catWomensClothes.category }}</h2>
+<img v-bind:src="catWomensClothes.image" :alt="catWomensClothes.title" class="categoryThumb">
 
 <div :key="item" v-for="item in products">
 <h3>{{ item.title }}</h3>
-<p>{{ item.id }}, {{ item.category }}</p>
 <img v-bind:src="item.image" :alt="item.title">
 </div>
 </template>
 
 <script>
-// const api = 'https://fakestoreapi.com/';
-// fetch('https://fakestoreapi.com/products/')
-//   .then((response) => response.json())
-//   .then((result) => {
-//     console.log(result);
-//     console.log(result[0]);
-//     for (let i = 1; i < result.length; i += 1) {
-//       if (result[i - 1].category === result[i].category) {
-//         console.log('samma');
-//       } else {
-//         console.log('inte samma');
-//       }
-//     }
-//   });
 
 export default {
   name: 'ProductCategories',
   data() {
     return {
-      productCategories: [],
       products: [],
     };
   },
@@ -45,42 +27,29 @@ export default {
     async fetchAllProducts() {
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
-      console.log(data);
       return data;
     },
   },
   async created() {
     this.products = await this.fetchAllProducts();
+    console.log(this.products);
   },
   computed: {
     // create filter function
-    // productCategories.filter
-    categoryMensClothes() {
-      // return this.products[1].category;
+    // to display each category of first item
+    catMensClothes() {
+      // return this.products[1].category, << first code
       return this.products.find((element) => element.category === "men's clothing");
     },
-    categoryMensClothesImage() {
-      return this.products[1].image;
-    },
-    categoryWomensClothes() {
-      return this.products[5].category;
-    },
-    categoryWomensClothesImage() {
-      return this.products[5].image;
+    categoryJewelry() {
+      return this.products.find((element) => element.category === 'jewelery');
     },
     categoryElectronics() {
       return this.products.find((element) => element.category === 'electronics');
     },
-    categoryElectronicsImage() {
-      return this.products[10].image;
+    catWomensClothes() {
+      return this.products.find((element) => element.category === "women's clothing");
     },
-    categoryJewelry() {
-      return this.products[15].category;
-    },
-    categoryJewelryImage() {
-      return this.products[15].image;
-    },
-
   },
 };
 
