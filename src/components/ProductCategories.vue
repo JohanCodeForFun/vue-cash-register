775016457eddf3e46066feb1df7ecca1044724f9
JohanCodeForFun <hellberg.johan@hotmail.com>
2022-11-23 22:24:35 +0100

    continued working on showing categories at the top of page, eliminating console errors.

--- src/components/ProductCategories.vue
+++ src/components/ProductCategories.vue
@@ -1,6 +1,13 @@
 <template>
-  <h2>{{ catMensClothes.category }}</h2>
-  <img v-bind:src="catMensClothes.image" :alt="catMensClothes.title" class="catThumb">
+  <button>Hello</button>
+  <h3>div med categories nedan</h3>
+  <div :key="item" v-for="item in categories">
+  <p>test</p>
+    <p>{{getCategories}}</p>
+  </div>
+  <h3>div med categories ovanför</h3>
+  <!-- <h2>{{ catMensClothes.category }}</h2>
+  <img v-bind:src="catMensClothes.image" :alt="catMensClothes.title" class="catThumb"> -->
   <!-- <h2>{{ categoryJewelry.category }}</h2>
   <img v-bind:src="categoryJewelry.image" :alt="categoryJewelry.title" class="catThumb">
   <h2>{{ categoryElectronics.category }}</h2>
@@ -23,6 +30,7 @@ export default {
   data() {
     return {
       products: [],
+      categories: [],
     };
   },
   methods: {
@@ -31,6 +39,21 @@ export default {
       const res = await fetch('https://fakestoreapi.com/products');
       const data = await res.json();
       return data;
+    },
+    getCategories() {
+      this.categories.push(this.products.find(
+        (element) => element.category === "men's clothing",
+      ));
+      console.log(this.categories);
+      // this.categories.push(this.products.find(
+      //   (element) => element.category === "men's clothing",
+      // ));
+      // this.categories.push(this.products.find(
+      //   (element) => element.category === "men's clothing",
+      // ));
+      // this.categories.push(this.products.find(
+      //   (element) => element.category === "men's clothing",
+      // ));
     },
     //   catch(err) {
     //     console.log(err);
@@ -61,6 +84,9 @@ export default {
     // catWomensClothes() {
     //   return this.products.find((element) => element.category === "women's clothing");
     // },
+  },
+  mounted() {
+    console.log('mounted log');
   },
 };
 
